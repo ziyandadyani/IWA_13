2769e058a21f16dda6b3076126bb8580500ccf66
Ziyanda Dyani <ziyandadyani17@gmail.com>
2023-05-02 14:59:55 +0200

    IWA 13 Challenge 1

--- script.js
+++ script.js
@@ -5,20 +5,25 @@ let calculated = '1'
 // Only allowed to change below
 
 
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
+const logCalc = () => { 
+    const isString = typeof calculated === 'string'  //= 'numerical-string'
+   //console.log(isString) 
+    let calculatedAsNumber
+   isString ?  calculatedAsNumber =  parseInt(calculated) : calculatedAsNumber= calculated 
+   //console.log(calculatedAsNumber)
+    calculated = calculatedAsNumber + 1 
 }
 
-const = calcUser () => {
-  logCalc
+//logCalc()
+
+const calcUser = () => {
+  logCalc()
   if (calculated > 2) user = 'John'
   if (calculated > 2) state = 'requesting'
   if (calculated > 3) state = 'idle'
 }
 
-const = checkUser () => {
+const checkUser=  () => {
 	if (user && state === 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
 	}
